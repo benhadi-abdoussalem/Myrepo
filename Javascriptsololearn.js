1643a8df157c151698a7426737198e429735325d
benhadi <dr.programme@gmail.com>
2024-02-03 18:15:04 +0100

    3/2/2024

--- Javascriptsololearn.js
+++ Javascriptsololearn.js
@@ -176,3 +176,37 @@ const obj1 = {
   };
   const obj2 = Object.assign({c: 5, d: 6}, obj1);
   console.log(obj2.c, obj2.d);
+  Array Destructuring in ES6
+
+/*The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+ES6 has added a shorthand syntax for destructuring an array.
+
+The following example demonstrates how to unpack the elements of an array into distinct variables:
+*/
+let arr = ['1', '2', '3'];
+let [one, two, three] = arr;
+
+console.log(one); // 1
+console.log(two); // 2
+console.log(three); // 3
+
+/*We can use also destructure an array returned by a function.
+
+For example:
+*/
+let a = () => {
+  return [1, 3, 2];
+};
+
+let [one, , two] = a();
+
+/*Notice that we left the second argument's place empty.
+
+The destructuring syntax also simplifies assignment and swapping values:
+
+*/
+let a, b, c = 4, d = 8;
+[a, b = 6] = [2]; // a = 2, b = 6
+
+[c, d] = [d, c]; // c = 8, d = 4
+/*Run the code and see how it works!*/
