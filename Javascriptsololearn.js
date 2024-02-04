dfe6dd3da50a7562ae3f6f1819859a46e334c7f3
benhadi <dr.programme@gmail.com>
2024-02-04 22:09:22 +0100

    4/2/2024

--- Javascriptsololearn.js
+++ Javascriptsololearn.js
@@ -279,3 +279,77 @@ function containsAll(arr, ...nums) {
 /*
 The ...nums parameter is called a rest parameter. It takes all the "extra" arguments passed to the function. The three dots (...) are called the Spread operator.
 Only the last parameter of a function may be marked as a rest parameter. If there are no extra arguments, the rest parameter will simply be an empty array; the rest parameter will never be undefined.*/
+
+//complete the function
+function Add(...myParams){
+  let myNumber = 0; 
+  for (let myParam of myParams) 
+  { myNumber += myParam; } 
+return myNumber; }
+console.log(Add(1,2,3));
+console.log(Add(4,14,5,9,14));
+console.log(Add(2,36));
+
+/*The Spread Operator
+This operator is similar to the Rest Parameter, but it has another purpose when used in objects or arrays or function calls (arguments).
+
+[h2]Spread in function calls[/h2]
+It is common to pass the elements of an array as arguments to a function. Before ES6, we used the following method:*/
+function myFunction(w, x, y, z) {
+  console.log(w + x + y + z);
+}
+var args = [1, 2, 3];
+myFunction.apply(null, args.concat(4));
+
+/*ES6 provides an easy way to do the example above with spread operators:
+*/
+const myFunction = (w, x, y, z) => {
+  console.log(w + x + y + z);
+};
+let args = [1, 2, 3];
+myFunction(...args, 4);
+
+/*Example:*/
+
+var dateFields = [1970, 0, 1];  // 1 Jan 1970
+var date = new Date(...dateFields);
+console.log(date);
+
+/*[h2]Spread in array literals[/h2]
+Before ES6, we used the following syntax to add an item at middle of an array:*/
+
+var arr = ["One", "Two", "Five"];
+
+arr.splice(2, 0, "Three");
+arr.splice(3, 0, "Four");
+console.log(arr);
+
+/*You can use methods such as push, splice, and concat, for example, to achieve this in different positions of the array. However, in ES6 the spread operator lets us do this more easily:*/
+
+let newArr = ['Three', 'Four']; 
+let arr = ['One', 'Two', ...newArr, 'Five'];
+console.log(arr);
+
+/*[h2]Spread in object literals[/h2]
+In objects it copies the own enumerable properties from the provided object onto a new object.
+*/
+const obj1 = { foo: 'bar', x: 42 };
+const obj2 = { foo: 'baz', y: 5 };
+
+const clonedObj = {...obj1}; // { foo: "bar", x: 42 }
+const mergedObj = {...obj1, ...obj2}; // { foo: "baz", x: 42, y: 5 }
+
+/*However, if you try to merge them you will not get the result you expected:*/
+*/
+const obj1 = { foo: 'bar', x: 42 };
+const obj2 = { foo: 'baz', y: 5 };
+const merge = (...objects) => ({...objects});
+
+let mergedObj = merge(obj1, obj2);
+// { 0: { foo: 'bar', x: 42 }, 1: { foo: 'baz', y: 5 } }
+
+let mergedObj2 = merge({}, obj1, obj2);
+// { 0: {}, 1: { foo: 'bar', x: 42 }, 2: { foo: 'baz', y: 5 } }
+
+//Shallow cloning or merging objects is possible with another operator called Object.assign().
+
