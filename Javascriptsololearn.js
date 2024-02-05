79d4819bc42fc8f341475611091b1e407445fc75
benhadi <dr.programme@gmail.com>
2024-02-05 20:47:35 +0100

    5/2/2024

--- Javascriptsololearn.js
+++ Javascriptsololearn.js
@@ -588,3 +588,35 @@ function main() {
       console.log(i[0]+" : "+i[1] )
   } 
 }
+/*ES6 Set
+A Set object can be used to hold unique values (no repetitions are allowed).
+A value in a set can be anything (objects and primitive values).
+
+The syntax new Set([iterable]) creates a Set object where iterable is an array or any other iterable object of values.
+
+The size property returns the number of distinct values in a set.
+*/
+let set = new Set([1, 2, 4, 2, 59, 9, 4, 9, 1]);
+
+console.log(set.size); // 5
+/*
+Methods
+add(value) Adds a new element with the given value to the Set.
+delete(value) Deletes a specified value from the set.
+has(value) Returns true if a specified value exists in the set and false otherwise.
+clear() Clears the set.
+values() Returns an Iterator of values in the set.
+*/
+let set = new Set();
+
+set.add(5).add(9).add(59).add(9);
+
+console.log(set.has(9));
+
+for (let v of set.values())
+  console.log(v);
+/*
+The above example demonstrates some of the ES6 Set methods.
+
+Set supports different data types i.e. 1 and "1" are two different values.
+NaN and undefined can also be stored in Set.*/
