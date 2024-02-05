e7e7110c9591d43a2f1ee2f03a0831e49bd99204
benhadi <dr.programme@gmail.com>
2024-02-05 20:41:24 +0100

    5/2/2024

--- Javascriptsololearn.js
+++ Javascriptsololearn.js
@@ -551,3 +551,40 @@ The above example demonstrates some of the ES6 Map methods.
 
 Map supports different data types i.e. 1 and "1" are two different keys/values.
 
+ES6 Map
+Five employees at a company are stored in Map in the program you are given. Their names are set as keys and their positions as values. The company is hiring one more employee. The program should take the name and the position as inputs and store them in the existing map.
+Complete the program to perform that operation and output to the console the list of employees in the format shown in the sample output.
+
+Sample Input
+Bob
+Developer
+
+Sample Output
+Richard : Developer
+Maria : SEO Specialist
+Tom : Product Manager
+David : Accountant
+Sophia : HR Manager
+Bob : Developer
+
+The entries() method returns an Iterator of array[key, value] in the map for each element.
+Don't forget to put spaces before and after the colon (:) in the output.
+*/
+function main() {
+  var name = readLine();
+  var position = readLine();
+  let employees = new Map([
+  ["Richard", "Developer"],
+  ["Maria", "SEO Specialist"],
+  ["Tom", "Product Manager"],
+  ["David", "Accountant"],
+  ["Sophia", "HR Manager"]
+  ]);
+
+  //add the new pair to the map
+  employees.set(name, position)
+  for (var i of employees.entries()){
+      //your code for the output
+      console.log(i[0]+" : "+i[1] )
+  } 
+}
