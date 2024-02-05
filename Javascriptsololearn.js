40a4e48ed23e9d2f4888843a5cec4b32a3de1982
benhadi <dr.programme@gmail.com>
2024-02-05 19:02:18 +0100

    5/2/2024

--- Javascriptsololearn.js
+++ Javascriptsololearn.js
@@ -353,3 +353,44 @@ let mergedObj2 = merge({}, obj1, obj2);
 
 //Shallow cloning or merging objects is possible with another operator called Object.assign().
 
+/*Classes in ES6
+In this lesson we'll explain how to create a class that can be used to create multiple objects of the same structure.
+A class uses the keyword class and contains a constructor method for initializing.
+
+For example:*/
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+/*A declared class can then be used to create multiple objects using the keyword new.
+For example:
+*/
+const square = new Rectangle(5, 5);
+const poster = new Rectangle(2, 3);
+
+/*Class Declarations are not hoisted while Function Declarations are. If you try to access your class before declaring it, ReferenceError will be returned.
+
+
+You can also define a class with a class expression, where the class can be named or unnamed.
+A named class looks like:
+*/
+var Square = class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+/*
+In the unnamed class expression, a variable is simply assigned the class definition:
+*/
+var Square = class {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+/*
+The constructor is a special method which is used for creating and initializing an object created with a class.
+There can be only one constructor in each class.
