5bac0d452ba382375c28a480395ba86fbb657ddb
benhadi <dr.programme@gmail.com>
2024-02-06 20:27:08 +0100

    6/2/2024

--- Javascriptsololearn.js
+++ Javascriptsololearn.js
@@ -620,3 +620,60 @@ The above example demonstrates some of the ES6 Set methods.
 
 Set supports different data types i.e. 1 and "1" are two different values.
 NaN and undefined can also be stored in Set.*/
+/*ES6 Promises
+A Promise is a better way for asynchronous programming when compared to the common way of using a setTimeout() type of method.
+
+Consider this example:
+*/
+setTimeout(function() {
+  console.log("Work 1");
+  setTimeout(function() {
+    console.log("Work 2");
+  }, 1000);
+}, 1000);
+console.log("End");
+/*
+It prints "End", "Work 1" and "Work 2" in that order (the work is done asynchronously). But if there are more events like this, the code becomes very complex.
+
+ES6 comes to the rescue in such situations. A promise can be created as follows:
+*/
+new Promise(function(resolve, reject) {
+    // Work
+    if (success)
+        resolve(result);
+    else
+        reject(Error("failure"));
+});
+/*
+Here, resolve is the method for success and reject is the method for failure.
+
+If a method returns a promise, its calls should use the then method which takes two methods as input; one for success and the other for the failure.
+
+For Example:
+*/
+function asyncFunc(work) {
+  return new Promise(function(resolve, reject) {
+    if (work === "")
+      reject(Error("Nothing"));
+    setTimeout(function() {
+      resolve(work);
+    }, 1000);
+  });
+}
+
+asyncFunc("Work 1") // Task 1
+.then(function(result) {
+  console.log(result);
+  return asyncFunc("Work 2"); // Task 2
+}, function(error) {
+  console.log(error);
+})
+.then(function(result) {
+  console.log(result);
+}, function(error) {
+  console.log(error);
+});
+console.log("End");
+/*
+It also prints "End", "Work 1" and "Work 2" (the work is done asynchronously). But, this is clearly more readable than the previous example and in more complex situations it is easier to work with.
+Run the code and see how it works!
