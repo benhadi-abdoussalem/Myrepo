c4c31b28c2029595bbec7a48c8290993fef98ffb
benhadi <dr.programme@gmail.com>
2024-02-06 21:28:17 +0100

    06/02/2024

--- Javascriptsololearn.js
+++ Javascriptsololearn.js
@@ -677,3 +677,53 @@ console.log("End");
 /*
 It also prints "End", "Work 1" and "Work 2" (the work is done asynchronously). But, this is clearly more readable than the previous example and in more complex situations it is easier to work with.
 Run the code and see how it works!
+/*Iterators & Generators
+Symbol.iterator is the default iterator for an object. The for...of loops are based on this type of iterator.
+
+In the example below, we will see how we should implement it and how generator functions are used.
+
+*/
+let myIterableObj = { 
+  [Symbol.iterator] : function* () {
+    yield 1; yield 2; yield 3;
+...
+console.log([...myIterableObj]);
+/*
+First, we create an object, and use the Symbol.iterator and generator function to fill it with some values.
+
+In the second line of the code, we use a * with the function keyword. It's called a generator function (or gen function).
+
+For example, here is a simple case of how gen functions can be useful:
+*/
+function* idMaker() {
+  let index = 0;
+  while (index < 5)
+    yield index++;
+}
+var gen = idMaker();
+console.log(gen.next().value);
+/*
+We can exit and re-enter generator functions later. Their variable bindings (context) will be saved across re-entrances. They are a very powerful tool for asynchronous programming, especially when combined with Promises. They can also be useful for creating loops with special requirements.
+
+We can nest generator functions inside each other to create more complex structures and pass them arguments while we are calling them.
+The example below will show a useful case of how we can use generator functions and Symbol.iterators together.
+
+Example:
+*/
+const arr = ['0', '1', '4', 'a', '9', 'c', '16'];
+const my_obj = {
+  [Symbol.iterator]: function*() {
+    for(let index of arr) {
+      yield `${index}`;
+    }
+  }
+};
+const all = [...my_obj]
+  .map(i => parseInt(i, 10))
+  .map(Math.sqrt)
+  .filter((i) => i < 5)
+  .reduce((i, d) => i + d);
+console.log(all);
+/*We create an object of 7 elements by using Symbol.iterator and generator functions. In the second part, we assign our object to a constant all. At the end, we print its value.
+
+Run the code and see how it works!
